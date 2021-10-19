6b0927a97b295b33f66a33375e77618ce80b9de8
Ian Mitchell <ian.mitchell@revature.net>
2021-10-19 12:50:41 -0600

    Loops

--- src/com/theIan/Main.java
+++ src/com/theIan/Main.java
@@ -1,8 +1,8 @@
-package com.theIan;
+package com.thevarungupta.Day1;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World");
     }
 }
