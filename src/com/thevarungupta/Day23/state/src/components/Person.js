749578188d0d479df2ad29abd3db21352ce57a24
Ian Mitchell <ian.mitchell@revature.net>
2021-11-19 10:11:30 -0700

    React.js state

---
+++ src/com/thevarungupta/Day23/state/src/components/Person.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class Person extends Component{
+
+    constructor(){
+        super()
+        this.state = {
+            firstName: 'Mark',
+            lastName: 'Smith',
+            age: 30,
+            country: 'USA'
+        }
+    }
+
+    onChange = () => {
+        //this.state.country = 'China'
+        this.setState({
+            country: 'China'
+        })
+        console.log('clicked')
+        console.log(this.state.country)
+    }
+
+    render(){
+        return <div>
+            <h1>Person component</h1>
+            <h2>Name: { this.state.firstName } { this.state.lastName }</h2>
+            <h2>I live in { this.state.country }</h2>
+            <button onClick={ this.onChange } >Change</button>
+        </div>
+    }
+}
+export default Person;
