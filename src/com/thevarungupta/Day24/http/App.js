e4942045e2634f1753175ee0b41b069e1438e2a3
Ian Mitchell <ian.mitchell@revature.net>
2021-11-22 15:30:11 -0700

    React.js http (axios)

---
+++ src/com/thevarungupta/Day24/http/App.js
@@ -0,0 +1,12 @@
+import Posts from "./components/Posts";
+
+function App() {
+  return (
+    <div className="container">
+     <h1>App Component</h1>
+      <Posts />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/com/thevarungupta/Day24/readme.txt
@@ -0,0 +1,14 @@
+# Conditional Rendering
+# List
+# Http call using axios
+
+
+# Conditional rendering
+- if / else
+- element value
+- turnery opertor
+- circuit breaker
+
+
+> npm i axios --save
+
